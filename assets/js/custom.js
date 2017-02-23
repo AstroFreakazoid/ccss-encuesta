167e96748785523624a70478ea989d783a708b44
MarvinJesus <mar90jesus@gmail.com>
2017-02-22 20:53:21 -0600

    update mananger page
    Actualizacion de la pagina manager.html

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -150,3 +150,132 @@ function initialize() {
 google.maps.event.addDomListener(window, "load", initialize);
  ========== END GOOGLE MAP ========== */
 
+
+
+ // ========== Logica formulario modal de registrarPregunta  ========== 
+//Funcion que hace tab para el registro de una nueva pregunta
+    $(".nuevaPregunta").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.1');
+        $active.next().removeClass('disabled');
+        nextTab($active);
+        $( '.wizard .nav-tabs li.1' ).addClass( "disabled" );
+        $( '.wizard .nav-tabs li.3' ).addClass( "disabled" );
+        $( '.wizard .nav-tabs li.4' ).addClass( "disabled" );
+   		limpiarText1();
+    });
+//Esta funcion limpia los input text de las preguntas y respuestas
+	function limpiarText1(){
+		$('#pregunta').val("");
+		$('#respA').val("");
+		$('#respB').val("");
+		$('#respC').val("");
+		$('#respD').val("");
+	}
+//Funcion que hace tab para el registro de un nuevo tema
+    $(".nuevoTema").click(function (e) {
+
+        var $active = $('.wizard .nav-tabs li.2');
+        prevTab($active);
+        $( '.wizard .nav-tabs li.1' ).removeClass('disabled');
+        $( '.wizard .nav-tabs li.2' ).addClass( "disabled" );
+        $( '.wizard .nav-tabs li.3' ).addClass( "disabled" );
+        $( '.wizard .nav-tabs li.4' ).addClass( "disabled" );
+        limpiarText2();
+    });
+
+//Esta funcion limpia los input text del tema,de las preguntas y respuestas
+	function limpiarText2(){
+		$('#tema').val("");
+		$('#pregunta').val("");
+		$('#respA').val("");
+		$('#respB').val("");
+		$('#respC').val("");
+		$('#respD').val("");
+		
+	}
+
+//Evento click del boton que registrar el tema, aqui se valida que el input text no este en blanco
+
+	$(".btnContinue1").click(function (e) {
+	    if ($('#tema').val() === ''|| $('#tema').val().indexOf(" ") == 0 ) {
+	       alertDGC('Ingrese el tema por favor');
+	    } else {
+	        var $active = $('.wizard .nav-tabs li.active');
+        	$active.next().removeClass('disabled');
+        	nextTab($active);
+	    }
+ 	});
+//Evento click del boton que registrar la pregunata, aqui se valida que el input text no este en blanco
+
+	$(".btnContinue2").click(function (e) {
+	    if ($('#pregunta').val() === ''|| $('#pregunta').val().indexOf(" ") == 0 ) {
+	       alertDGC('Ingrese la pregunta por favor');
+	    } else {
+	        var $active = $('.wizard .nav-tabs li.active');
+        	$active.next().removeClass('disabled');
+        	nextTab($active);
+	    }
+ 	});
+
+//Evento click del boton que registrar las repuestas, aqui se valida que los input text no esten en blanco.
+
+	$(".btnContinue3").click(function (e) {
+		var siguiente=true;
+
+	    if ($('#respA').val() === ''|| $('#respA').val().indexOf(" ") == 0 ) {
+	       alertDGC('Ingrese la respuesta A');
+	       siguiente=false;
+	    }
+	    if ($('#respB').val() === ''|| $('#respB').val().indexOf(" ") == 0 ) {
+	       alertDGC('Ingrese la respuesta B');
+	       siguiente=false;
+	    } 
+	    if ($('#respC').val() === ''|| $('#respC').val().indexOf(" ") == 0 ) {
+	       alertDGC('Ingrese la respuesta C');
+	       siguiente=false;
+	    } 
+	    if ($('#respD').val() === ''|| $('#respD').val().indexOf(" ") == 0 ) {
+	       alertDGC('Ingrese la respuesta D');
+	       siguiente=false;
+	    }
+
+	    if(siguiente===true){
+	        var $active = $('.wizard .nav-tabs li.active');
+        	$active.next().removeClass('disabled');
+        	nextTab($active);
+	    }
+ 	});
+
+
+//Inicio Alert
+	function alertDGC(mensaje)
+	{
+	    var dgcTiempo=500
+	    var ventanaCS='<div class="dgcAlert"><div class="dgcVentana"><div class="dgcCerrar"></div><div class="dgcMensaje">'+mensaje+'<br><div class="dgcAceptar">Aceptar</div></div></div></div>';
+	    $('body').append(ventanaCS);
+	    var alVentana=$('.dgcVentana').height();
+	    var alNav=$(window).height();
+	    var supNav=$(window).scrollTop();
+	    $('.dgcAlert').css('height',$(document).height());
+	    $('.dgcVentana').css('top',((alNav-alVentana)/2+supNav-100)+'px');
+	    $('.dgcAlert').css('display','block');
+	    $('.dgcAlert').animate({opacity:1},dgcTiempo);
+	    $('.dgcCerrar,.dgcAceptar').click(function(e) {
+	        $('.dgcAlert').animate({opacity:0},dgcTiempo);
+	        setTimeout("$('.dgcAlert').remove()",dgcTiempo);
+	    });
+	}
+	window.alert = function (message) {
+	  alertDGC(message);
+	};
+
+//Fin Alert
+
+	function nextTab(elem) {
+	    $(elem).next().find('a[data-toggle="tab"]').click();
+	}
+	function prevTab(elem) {
+	    $(elem).prev().find('a[data-toggle="tab"]').click();
+	}
+ // ========== Logica formulario modal de registrarPregunta  ========== 
