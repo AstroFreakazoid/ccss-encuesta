b1aa5b37ea7fe7cff59456ea2a0b98095e5a37b7
MarvinJesus <mar90jesus@gmail.com>
2017-02-23 21:27:26 -0600

    update page
    mananger.html

--- assets/angular/appController.js
+++ assets/angular/appController.js
@@ -1,108 +1,73 @@
-/**
- * Created by alego on 30/1/2017.
- */
-angular.module('myApp', [])
-    .controller('encuestaCtrl', function($scope) {
-        $scope.topics = 
-            [
-                {
-                    id: 'q_1',
-                    id_container: 'content3',
-                    name: 'GERIATRIA',
-                    questions: 
-                    [
-                        {
-                            id: 'q1-1',
-                            title:'DONDE VIVES',
-                            answers:['GUNACASTE','LIMON','HEREDIA','GRECIA'],
-                            options:['A','B','C','D'],
-                            correctAnswer:3
-                        },
-                        {
-                            id: 'q1-2',
-                            title:'COMO ES EL MUNDO',
-                            answers:['LINDO','LOCO','VERDE','TRISTE'],
-                            options:['A','B','C','D'],
-                            correctAnswer:3
-                        },
-                        {
-                            id: 'q1-3',
-                            title:'COMO ERES TU',
-                            answers:['Es esto','Es quello','Es Amor','Es Ciencia'],
-                            options:['A','B','C','D'],
-                            correctAnswer:3
-                        }
-                    ]
-                },
-                {
-                    id: 'q_2',
-                    id_container: 'content2',
-                    name: 'GERONTOLOGIA',
-                    questions: 
-                    [
-                        {
-                            id: 'q2-1',
-                            title:'QUE ES GEREATRIA',
-                            answers:['Cuando','DONDE','PORQUE','Es Ciencia'],
-                            options:['A','B','C','D'],
-                            correctAnswer:3
-                        },
-                        {
-                            id: 'q2-2',
-                            title:'QUE ES NATURALEZA',
-                            answers:['Es esto','Es quello','Es Amor','Es Ciencia'],
-                            options:['A','B','C','D'],
-                            correctAnswer:3
-                        },
-                        {
-                            id: 'q2-3',
-                            title:'QUE ES  AGUA',
-                            answers:['LIQUIDO','H2O','Es Amor','REFRESCANTE'],
-                            options:['A','B','C','D'],
-                            correctAnswer:3
-                        }
-                    ]    
-                },
-                {
-                    id: 'q_3',
-                    id_container: 'content3',
-                    name: 'GERIATRIA',
-                    questions: 
-                    [
-                        {
-                            id: 'q3-1',
-                            title:'DONDE VIVES',
-                            answers:['GUNACASTE','LIMON','HEREDIA','GRECIA'],
-                            options:['A','B','C','D'],
-                            correctAnswer:3
-                        },
-                        {
-                            id: 'q3-2',
-                            title:'COMO ES EL MUNDO',
-                            answers:['LINDO','LOCO','VERDE','TRISTE'],
-                            correctAnswer:3
-                        },
-                        {
-                            id: 'q3-3',
-                            title:'COMO ERES TU',
-                            answers:['Es esto','Es quello','Es Amor','Es Ciencia'],
-                            options:['A','B','C','D'],
-                            correctAnswer:3
-                        }
-                    ]
-                },
-            ];
+//Se inicializa el usuario
+const userAdmin='mar90jesusgmailcom';
+//Se inicializa el usuario
+(function(){
+    //Se inicialializa firebase
+    var config = {
+        apiKey: "AIzaSyCmI3gN4jJR-TV7FaiGhUkhvxOdtdP2sco",
+        authDomain: "encuesta-5a920.firebaseapp.com",
+        databaseURL: "https://encuesta-5a920.firebaseio.com",
+        storageBucket: "encuesta-5a920.appspot.com",
+        messagingSenderId: "843177083745"
+  };
+  firebase.initializeApp(config);
 
-    })
-    .controller('managerCtrl', function($scope) {            
-        $scope.userAuth = {
-            username: "MARVIN",
-            password: "1723hjasd",
-            mail: "marvin@astrofreakazoid.com"
-        }
-        $scope.invitado = {
-            username: "invitado",
-            password: "12345",
-            mail: "marvin@astrofreakazoid.com"
-        }
+  //Se inicialializa firebase
+
+  //Se crean el modulo y los controller
+    angular
+    .module('myApp',['firebase'])
+
+    .controller('MyCtrl',function($scope,$firebaseObject){
+        //Se declara la variable del usuario invitado
+        $scope.userinvited='null';
+        //Se crea una referencia a la informacion extra relacionada a la pagina mananger
+        const referenciaInformation=firebase.database().ref().child('information/infoMananger/questionsrgistr');
+        //Se crea un objeto con la funcion$firebaseObject();
+        this.objectInform=$firebaseObject(referenciaInformation);
+        //Se crea una referencia a la informacion relacionda con un administrados
+        const referenciaUserAdmin=firebase.database().ref().child('uses/admin');
+        //Se crea la rferencia a los datos del usuario administrados
+        const userRef=referenciaUserAdmin.child(userAdmin);
+        const profileRef=userRef.child('profile');
+        //Se crea un objeto con la funcion$firebaseObject();
+        this.UserAdmidObject=$firebaseObject(profileRef);
+        //Se obtiene la referencia al nombre del usuario invitado 
+        userinvitedRef=profileRef.child('invitedname');
+        //Se declara la variable que guardara la referencia a los datos del invitado
+        var referenciaUserInvited;
+        //Se corre un intervalo de tiempo a segundo=10000ms. Para validar si hay invitado registrado 
+        setInterval(function(){
+            //Se ejecuta la function apply() para que este al tanto de culaquier cambio con el invitado
+            $scope.$apply(function(){
+                //Se obtienen el nombre del invitado pormedio del snapshot
+                userinvitedRef.on('value', function(snapshot) {
+                //Se crea un objeto invitado. Por medio de la referncia se trae el Json
+                  referenciaUserInvited=firebase.database().ref().child('uses/invited/'+snapshot.val());
+                  //Se valida si existe  invitado sino para ocultar el boton de registrar invitado
+                  if(snapshot.val()!='null'){
+                     this.UserInvietdObject=$firebaseObject(referenciaUserInvited);
+                     $('#btnRegistrarInvitado').addClass("hidden");//Se oculta el boton
+                     $('#btnDatosInvitado').removeClass("hidden");//Remueve la clase que oculta el boto
+                    $('#btnDatosInvitado').addClass("show");//Agrega una clase que hace el boton visible
+                  }
+                 if(snapshot.val()==='null'){
+                    $('#btnDatosInvitado').addClass("hidden");//Remueve la clase que oculta el boto
+                    $('#btnRegistrarInvitado').removeClass("hidden");//Agrega la clase que oculta el boto
+                    $('#btnRegistrarInvitado').addClass("show");//Agrega una clase que hace el boton visible
+                  }
+                });
+            });
+        },1000);
+        //Se crea un array con datos quemados. Es el array avecedario.
+        $scope.obtion= [{id:'A'},{id:'B'},{id:'C'},{id:'D'},{id:'E'},{id:'F'},{id:'G'},{id:'H'},{id:'I'},
+        {id:'J'},{id:'K'},{id:'L'},{id:'M'},{id:'N'},{id:'Ñ'},{id:'O'},{id:'P'},
+        {id:'Q'},{id:'R'},{id:'S'},{id:'T'},{id:'U'},{id:'V'},{id:'W'},{id:'X'},{id:'Y'},{id:'Z'}];
+        //Seccion para la extracion de los datos de los temas.
+        const topicsRef=userRef.child('topics');
+        //Se crea un objeto topics. Por medio de la referncia se trae el Json
+        this.topicObject=$firebaseObject(topicsRef);
+              
     });
+
+}());
